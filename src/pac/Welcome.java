20770072b9b7d5d18b30c91c32ab08c713f0c076
Dinesh <dineshpanda.369@gmail.com>
2020-12-19 11:36:17 +0530

    3rd commit

--- src/pac/Welcome.java
+++ src/pac/Welcome.java
@@ -2,6 +2,10 @@ package pac;
 
 public class Welcome {
 public static void main(String[] args) {
-	
+int x=10;
+int y=20;
+System.out.println("Sum of 2 nos is"+" "+(x+y));
+
+System.out.println("Welcome");
 }
 }
